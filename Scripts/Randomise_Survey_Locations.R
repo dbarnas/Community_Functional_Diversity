53dc7f72bb95ca84e7ca7bc445008733ee2a4039
Danielle Barnas <danielle.barnas@csun.edu>
2022-04-14 15:45:01 -0700

    update random cabral survey pts and cluster analysis

--- Scripts/Randomise_Survey_Locations.R
+++ Scripts/Randomise_Survey_Locations.R
@@ -9,8 +9,9 @@ library(ggmap)
 library(maptools)
 library(kriging)
 library(sf)
+library(patchwork)
 
-# 
+#
 # library(patchwork)
 # library(viridis)
 # library(ggnewscale)
@@ -36,10 +37,14 @@ SiteGPS<-AllChemData %>%
   group_by(Location, CowTagID) %>%
   summarise(lon = mean(lon, na.rm = TRUE),
             lat = mean(lat, na.rm = TRUE))
+VGPS <- SiteGPS %>%
+  filter(Location == "Varari", CowTagID != "Varari_Well")
+CGPS <- SiteGPS %>%
+  filter(Location == "Cabral", CowTagID != "CSPRING")
 
 # Base Maps
-API<-names(read_table(here("Data","API.txt")))
-register_google(key = API) ### use your own API
+# API<-names(read_table(here("Data","API.txt")))
+# register_google(key = API) ### use your own API
 
 # Varari
 #VarariBaseMap<-get_map(MeanGPS %>% filter(Location == "Varari") %>% select(lon,lat), maptype = 'satellite', zVaraoom = 18)
@@ -56,9 +61,10 @@ C_kml <- getKMLcoordinates(kmlfile=here("Data","Polygons","Cabral_polygon.kml"),
 
 ## RANDOM VARARI POINTS
 
-# R will pick the 7th random set of points every time (gives me static "random" points, 
+# R will pick the 7th random set of points every time (gives me static "random" points,
 # so I don't get different points every time I run)
-set.seed(7) 
+# set.seed(7) for Varari, but set.seed(2) for Cabral to have different, more even distribution
+set.seed(7)
 
 V_kml_sf<-sf::st_read(here("Data","Polygons","Varari_Polygon.kml")) #read in kml polygon file as a shape file (sf)
 myrandomV<-sf::st_sample(V_kml_sf, size=80, type="random") #sample random points from polygon shape file
@@ -76,6 +82,8 @@ myrandomVpts<-data.frame(myrandomVpts) %>% # parse to dataframe and rename colum
   rename(lon="Vlon_list", lat="Vlat_list")
 
 ## RANDOM CABRAL POINTS
+set.seed(2)
+
 C_kml_sf<-sf::st_read(here("Data","Polygons","Cabral_Polygon.kml")) #read in kml polygon file as a shape file (sf)
 myrandomC<-sf::st_sample(C_kml_sf, size=80, type="random") #sample random points from polygon shape file
 myrandomC
@@ -92,37 +100,77 @@ myrandomCpts<-data.frame(myrandomCpts) %>% # parse to dataframe and rename colum
   rename(lon="Clon_list", lat="Clat_list")
 
 ## Join points of both locations
-Vpts<-myrandomVpts %>% 
+Vpts<-myrandomVpts %>%
   mutate(Location = "Varari") %>% # add location column
   arrange(lat) # organize data from south to north
-Cpts<-myrandomCpts %>% 
-  mutate(Location = "Cabral") %>% 
+Cpts<-myrandomCpts %>%
+  mutate(Location = "Cabral") %>%
   arrange(lat)
-Rpts<-Vpts %>% 
-  full_join(Cpts)
-write_csv(Rpts, here("Data","Randomized_Locations.csv"))
+Rpts<-Vpts %>%
+  full_join(Cpts) %>%
+  mutate(CowTagID = paste0("random_",rownames(Rpts)))
+Allpts <- Rpts %>%
+  full_join(VGPS) %>%
+  full_join(CGPS)
+
+write_csv(Allpts, here("Data","Randomized_Locations.csv"))
 ## Import csv into Google Earth to visualize
 
 
 # Maps with random points
+
+Allpts <- read_csv(here("Data","Randomized_Locations.csv"))
+
 # Varari
-VarariRandomMap<-get_map(myrandomVpts %>% select(lon,lat), 
-                         maptype = 'satellite', zoom = 18)
+VarariRandomMap<-get_map(MeanGPS %>% filter(Location == "Varari") %>% select(lon,lat) %>% mutate(lon = lon + 0.00005, lat = lat + 0.0001), #Allpts %>% filter(Location == "Varari") %>% select(lon,lat),
+                         maptype = 'satellite', zoom = 19)
+VSeepPt<-VGPS %>% filter(CowTagID == "VSEEP") # isolate Varari seep
 VMap<-ggmap(VarariRandomMap) +
-  geom_point(data=myrandomVpts, aes(x=lon, y=lat, colour="red"), 
-             size=2, alpha=0.8) +
-  theme(legend.position = "none")
+  geom_point(data=VGPS, aes(x=lon, y=lat),
+             size=4, shape = 23, alpha = 0.8, fill = "brown2", color = "black") + # selected 20 points
+  geom_point(data=Vpts, aes(x=lon, y=lat),
+             size=4, shape = 21, alpha = 0.8, fill = "white", color = "black") + # randomized points
+  geom_point(data=VSeepPt, aes(x=lon, y=lat),
+             size=4, shape = 8, color = "yellow") + # seepage point
+  theme(legend.position = "none") +
+  labs(x = "Longitude", y = "Latitude")
+VMap
+
 ggsave(here("Output","Random_Varari_map.pdf"),VMap, "pdf", width=6.88, height=4.35, units="in")
+ggsave(here("Output","Random_Varari_map.png"),VMap, "png", width=6.88, height=4.35, units="in")
 
 # Cabral
-CabralRandomMap<-get_map(myrandomCpts %>% select(lon,lat), 
-                         maptype = 'satellite', zoom = 18)
+CabralRandomMap<-get_map(MeanGPS %>% filter(Location == "Cabral") %>% select(lon,lat) %>% mutate(lon = lon + 0.00005, lat = lat - 0.00008),
+                                 maptype = 'satellite', zoom = 18)
+CSeepPt<-CGPS %>% filter(CowTagID == "CSEEP") # isolate Varari seep
 CMap<-ggmap(CabralRandomMap) +
-  geom_point(data=myrandomCpts, aes(x=lon, y=lat, colour="red"), 
-             size=2, alpha=0.8) +
-  theme(legend.position = "none")
-ggsave(here("Output","Random_Cabral_maps.pdf"), CMap, width=6.88, height=4.35, units="in")
+  geom_point(data=CGPS, aes(x=lon, y=lat),
+             size=4, shape = 23, alpha = 0.8, fill = "brown2", color = "black") + # selected 20 points
+  geom_point(data=Cpts, aes(x=lon, y=lat),
+             size=4, shape = 21, alpha = 0.8, fill = "white", color = "black") + # randomized points
+  geom_point(data=CSeepPt, aes(x=lon, y=lat),
+             size=4, shape = 8, color = "yellow") + # seepage point
+  theme(legend.position = "none") +
+  labs(x = "Longitude", y = "Latitude")
+CMap
 
+ggsave(here("Output","Random_Cabral_maps.pdf"), CMap, "pdf", width=6.88, height=4.35, units="in")
+ggsave(here("Output","Random_Cabral_maps.png"), CMap, "png", width=6.88, height=4.35, units="in")
+
+## Patchwork Maps
+
+SurveyMaps<-VMap + CMap +
+  plot_layout(nrow = 1) +
+  patchwork::plot_annotation(tag_levels = 'A',
+                             theme = theme(plot.title = element_text(size = rel(1.5),
+                                                                     face = "bold",
+                                                                     hjust = 0.5,
+                                                                     margin = margin(t = 10, b = 20,
+                                                                                     unit = "pt"))))
+SurveyMaps
+
+ggsave(plot = SurveyMaps, filename = here("Output","Random_Survey_Maps.pdf"), width = 14, height = 10)
+ggsave(plot = SurveyMaps, filename = here("Output","Random_Survey_Maps.png"), width = 14, height = 10)
 
 
 
