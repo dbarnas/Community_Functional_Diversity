8dc1a735b5e12a5e3818e64e0e86ebb85fe1746d
Danielle Barnas <danielle.barnas@csun.edu>
2022-08-25 16:10:52 -0700

    nutrient/biogeochem processing script. removes redundancy in other scripts

---
+++ Scripts/August_Nutrient_Processing.R
@@ -0,0 +1,82 @@
+#### Process August nutrient data and save as csv
+#### Created by Danielle Barnas
+#### Created on 8/25/2022
+
+## Load Libraries
+library(tidyverse)
+library(lubridate)
+library(here)
+library(curl) # pull data from url
+
+
+## Read in data
+AllChemData<-read_csv(curl('https://raw.githubusercontent.com/njsilbiger/MooreaSGD_site-selection/main/Data/August2021/Allbiogeochemdata_QC.csv'))
+turb1<-read_csv(here("Data","Biogeochem","August2021","Turb_NC.csv"))
+
+
+# AllChemData<-read_csv(here("Data","Biogeochem","March2022"))
+# turb1<-read_csv(here("Data","Biogeochem","March2022","Turb_NC.csv"))
+
+
+
+## Clean Data
+## remove unnecessary/redundant turbinaria data to not include in cluster analysis
+turb <- turb1 %>%
+  select(CowTagID, del15N, N_percent)
+
+
+## There seems to be a contaminated nutrient sample for V2 Low tide on the 8/8/2021.  Remove this point
+removeSite1 <- AllChemData %>%
+  filter(CowTagID == "V2",
+         Tide == " Low",
+         Day_Night == "Day",
+         Date == ymd("2021-08-08"))
+
+removeSite2 <- AllChemData %>%
+  filter(CowTagID == "C4",
+         Tide =="Low",
+         Day_Night == "Night",
+         Date == ymd("2021-08-09"))
+
+## Filter out redundant low tide day sample, first 'low tide' was super high
+removeSite3 <- AllChemData %>%
+  filter(Tide == "Low",
+         Day_Night == "Day",
+         Date == ymd("2021-08-06"))
+
+## Remove unnecessary/redundant data
+ReducedChemData <- AllChemData %>%
+  # remove outlier data and bad sampling date
+  anti_join(removeSite1) %>%
+  anti_join(removeSite2) %>%
+  anti_join(removeSite3) %>%
+  # only keep Plate and Seep data
+  filter(Plate_Seep %in% c("Plate", "Seep")) %>%  # ignores springs and well
+  # remove unnecessary columns
+  select(-c(Date,
+            Time,
+            DateTime,
+            Plate_Seep,
+            Top_Plate_ID,
+            Bottom_Plate_ID,
+            Jamie_Plate_ID,
+            Temperature,
+            MarineHumic_Like, Lignin_Like)) # Craig recommends to remove "because we don't hypothesize them to be orthogonal to any of the other fDOM we're using"
+
+
+## Summarise: Range of parameters
+
+Full_data <- ReducedChemData %>%
+  group_by(Location, CowTagID) %>%
+  # get parameter ranges across sampling periods by site and plate
+  summarise_at(vars(Salinity:Tyrosine_Like), .funs = range, na.rm=T) %>% # returns two rows containing max and min value per CowTagID
+  summarise_at(vars(Salinity:Tyrosine_Like), .funs = diff, na.rm=T) %>%  # returns difference between rows per CowTagID (yields range at each location)
+  ungroup() %>%
+  left_join(turb, by = 'CowTagID') # join with turb
+
+Full_data
+
+write_csv(Full_data, here("Data","Biogeochem","AugNutrient_Processed.csv"))
+
+
+
---
+++ Scripts/March_Nutrient_Processing.R
@@ -0,0 +1,71 @@
+#### Process August nutrient data and save as csv
+#### Created by Danielle Barnas
+#### Created on 8/25/2022
+
+## Load Libraries
+library(tidyverse)
+library(lubridate)
+library(here)
+library(curl) # pull data from url
+
+
+## Read in data
+AllChemData <- read_csv(curl('https://raw.githubusercontent.com/njsilbiger/MooreaSGD_site-selection/main/Data//March2022/CarbonateChemistry/pHProbe_Data_calculated_POcorrect.csv'))
+turb1<-read_csv(here("Data","Biogeochem","March2022","Turb_NC.csv"))
+gps <- read_csv(here("Data","Sandwich_Locations_Final.csv"))
+
+
+## Clean Data
+## remove unnecessary/redundant turbinaria data to not include in cluster analysis
+turb <- turb1 %>%
+  select(CowTagID, del15N, N_percent)
+
+## isolate lat and lon to later join
+gps <- gps %>%
+  select(CowTagID, lat, lon)
+
+
+## Filter out unnecessary/redundant data
+removeSite <- AllChemData %>%
+  select(CowTagID) %>%
+  filter(CowTagID %in% c("VSPRING", # sample locations unnecessary for my analysis
+                         "VRC",
+                         "CRC",
+                         "CPIT",
+                         "CPIT_Bottom",
+                         "CSPRING_ROAD",
+                         "CSPRING_BEACH",
+                         "CSPRING_BEACH2"))
+
+
+ReducedChemData <- AllChemData %>%
+  # remove unnecessary columns
+  select(-c(TempInSitu
+            #MarineHumic_Like,
+            #Lignin_Like # Craig recommends to remove "because we don't hypothesize them to be orthogonal to any of the other fDOM we're using"
+  )) %>%
+  mutate(Location = if_else(str_detect(CowTagID, "^V") == T, "Varari", "Cabral")) # if CowTagID starts with V, label location as varari, else (if starts with C) label as Cabral
+
+
+## Summarise: Range of parameters
+
+Full_data <- ReducedChemData %>%
+  group_by(Location, CowTagID) %>%
+  # get parameter ranges across sampling periods by site and plate
+  summarise_at(vars(Salinity:Phosphate_umolL), .funs = range, na.rm=T) %>% # returns two rows containing max and min value per CowTagID
+  summarise_at(vars(Salinity:Phosphate_umolL), .funs = diff, na.rm=T) %>%  # returns difference between rows per CowTagID (yields range at each location)
+  ungroup() %>%
+  left_join(turb, by = 'CowTagID') %>%  # join with turb
+  # only keep Plate and Seep data
+  anti_join(removeSite) # ignores springs, pits, and offshore samples
+
+
+
+
+
+
+
+write_csv(Full_data, here("Data","Biogeochem","MarchNutrient_Processed.csv"))
+
+
+
