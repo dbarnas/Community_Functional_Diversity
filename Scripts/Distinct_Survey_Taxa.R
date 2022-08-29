1558b01ea599060aec31276b7442bee941c78908
Danielle Barnas <danielle.barnas@csun.edu>
2022-08-29 15:57:45 -0700

    script to join functional traits with distinct taxa from surveys

---
+++ Scripts/Distinct_Survey_Taxa.R
@@ -0,0 +1,40 @@
+
+
+#### LIBRARIES ####
+library(tidyverse)
+library(here)
+library(tidytext)
+
+
+
+#### READ IN DATA ####
+survey <- read_csv(here("Data","Surveys","Species_Composition_2022.csv"))
+
+### Both Survey locations
+taxa_a <- read_csv(here("Data", "Surveys", "Distinct_Taxa.csv"))
+
+a <- survey %>%
+  distinct(Taxa)
+
+### Join with functional trait dataframe
+taxa_a <- taxa_a %>%
+  full_join(a)
+
+### Write over csv
+write_csv(taxa, here("Data", "Surveys", "Distinct_Taxa.csv"))
+
+
+### Varari only
+taxa_b <- read_csv(here("Data", "Surveys", "Distinct_Varari_Taxa.csv"))
+
+b <- survey %>%
+  filter(Location != "Cabral") %>%
+  distinct(Taxa)
+
+### Join with functional trait dataframe
+taxa_b <- taxa_b %>%
+  full_join(b)
+
+
+### Write over csv
+write_csv(taxa_b, here("Data", "Surveys", "Distinct_Varari_Taxa.csv"))
