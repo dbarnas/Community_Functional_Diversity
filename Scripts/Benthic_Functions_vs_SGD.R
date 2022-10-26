07e1137da57e082b0e81a219c380e470438afd72
Danielle Barnas <danielle.barnas@csun.edu>
2022-10-26 14:30:31 -0700

    update to view with summer TO data and relative to distance from seep

--- Scripts/Benthic_Functions_vs_SGD.R
+++ Scripts/Benthic_Functions_vs_SGD.R
@@ -13,6 +13,7 @@ library(ggrepel)
 taxa <- read_csv(here("Data", "Surveys","Distinct_Taxa.csv"))
 survey <- read_csv(here("Data", "Surveys", "Species_Composition_2022.csv"))
 nutrient <- read_csv(here("Data", "Biogeochem", "July2022", "Turb_NC.csv"))
+meta <- read_csv(here("Data", "Full_Metadata.csv"))
 
 
 #### CLEAN DATA ####
@@ -22,37 +23,39 @@ funTrait <- taxa %>%
   mutate(coral_algae = if_else(Taxon_Group == "Rhodophyta", true = "Macroalgae", false = if_else(
                                  Taxon_Group == "Chlorophyta", true = "Macroalgae", false = if_else(
                                  Taxon_Group == "Phaeophyta", true = "Macroalgae", false = Taxon_Group))))
+dist <- meta %>%
+  select(CowTagID, dist_to_seep_m)
 
 # calculate percent cover of primary taxon groups
 pCover <- funTrait %>%
   full_join(survey) %>%
   group_by(Location, CowTagID) %>%
   count(coral_algae) %>%
+  ungroup() %>%
   pivot_wider(names_from = coral_algae, values_from = n) %>%
-  mutate(total = sum(across(Abiotic:Exposed), na.rm = T)) %>% # calculate total individuals per category
-  mutate(across(Abiotic:Exposed, ~ . / total * 100), .keep = c("all")) %>%  # calculate % cover for benthic categories per cowtagid
-  select(-total) %>%   # remove intermediate metric
-  pivot_longer(Abiotic:Exposed, names_to = "Taxa", values_to = "values") %>% # pivot to replace na with zero
-  mutate(values = if_else(is.na(values), 0, values)) %>%
-  pivot_wider(names_from = Taxa, values_from = values) # pivot back to wide
+  mutate_at(vars('Abiotic':'Cyanobacteria'), .funs = as.double) %>% # need to parse from integer to double
+  mutate_at(vars('Abiotic':'Cyanobacteria'), .funs = ~if_else(is.na(.) == TRUE, 0, .)) %>% # change NAs to 0
+  group_by(Location,CowTagID) %>%
+  mutate(total = sum(across(Abiotic:Cyanobacteria), na.rm = T)) %>% # calculate total individuals per category
+  mutate(across(Abiotic:Cyanobacteria, ~ . / total * 100), .keep = c("all")) %>%  # calculate % cover for benthic categories per cowtagid
+  select(-total) # remove intermediate metric
 
 # binning calcifiers and fleshy+turf algae
 pCover <- pCover %>%
   mutate(TotalCalc = CCA + Coral,
-         TotalAlgae = Macroalgae + Turf + Abiotic)
-
-# isolate t ornata nitrogen data
-nutrient <- nutrient %>%
-  mutate(Location = "Varari",
-         Season = "Wet") %>%
-  distinct()
+         TotalAlgae = Macroalgae + Turf) # + Abiotic)
 
 # join with biogeochemical data
 pCover <- pCover %>%
-  filter(CowTagID != "VSEEP", CowTagID != "CSEEP") %>%
-  filter(Location == "Varari") %>%  # for summer 2022 turbinaria
+  mutate(Location = if_else(Location == "Varari_Maya", "Varari", Location)) %>%
   left_join(nutrient) %>%
-  drop_na(Season)
+  filter(CowTagID != "VSEEP", CowTagID != "CSEEP") %>%
+  drop_na(N_percent)
+
+#write_csv(pCover, here("Data", "Biogeochem", "July2022", "Calcifier_Nutrients_June2022.csv"))
+
+distCover <- pCover %>%
+  right_join(dist)
 
 
 #### PLOTTING ####
@@ -62,21 +65,67 @@ pCover <- pCover %>%
 pCover %>%
   ggplot(aes(x = TotalCalc, y = TotalAlgae)) +
   geom_point() +
-  geom_text(aes(label = ifelse(TotalCalc < 5, CowTagID, NA)),
+  geom_smooth(method = "lm") +
+  geom_text(aes(label = CowTagID),
             nudge_y = -1, nudge_x = 1, size = 3) +
   theme_bw() +
   theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-  #facet_wrap(~Season)
+        panel.grid.minor = element_blank()) +
+  facet_wrap(~Location)
+anova(lm(data = pCover, TotalCalc ~ TotalAlgae))
 
-# Remove outliers
-redCover <- pCover %>%
-  filter(CowTagID != "V17")
+pCover <- pCover %>%
+  filter(CowTagID != "Sand_SGD_2",
+         CowTagID != "Sand_SGD_1",
+         CowTagID != "V17") # remove outliers 0 or near 0% calcifiers)
+
+
+## Preview nutrient loading at survey locations
+pCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = N_percent), y = N_percent)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+pCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = C_N), y = C_N)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+pCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = del15N), y = del15N)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+
+## Preview with distance to seep
+distCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = dist_to_seep_m), y = N_percent)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+distCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = dist_to_seep_m), y = C_N)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+distCover %>%
+  ggplot(aes(x = fct_reorder(.f = CowTagID, .x = dist_to_seep_m), y = del15N)) +
+  geom_point() + labs(x = "CowTagID") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+
+distCover %>%
+  pivot_longer(cols = c(del15N:N_percent), names_to = "Nutrients", values_to = "Values") %>%
+  ggplot(aes(x = dist_to_seep_m, y = Values)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90)) +
+  facet_wrap(~Nutrients, scales = "free_y") +
+  geom_text_repel(aes(label = CowTagID))
+
+anova(lm(data = distCover, N_percent ~ dist_to_seep_m))
+anova(lm(data = distCover, C_N ~ dist_to_seep_m))
+anova(lm(data = distCover, del15N ~ dist_to_seep_m))
+
 
 #### Comparing coral/algae ratios across sgd ####
 
 # del15N
-p1 <- redCover %>%
+p1 <- pCover %>%
   ggplot(aes(y = log((TotalCalc+1)/(TotalAlgae+1)), x = del15N)) +
   geom_point() +
   geom_text_repel(aes(label = CowTagID), size = 3) +
@@ -88,17 +137,17 @@ p1 <- redCover %>%
   labs(y = "log ratio of calcifiers to fleshy algae",
        color = "",
        x = "del15N (Nutrient Source)")+
-  annotate("text", x = 4.8, y = 0.3, label = "Calcifier-dominated")+
-  annotate("text", x = 4.8, y = -0.3, label = "Fleshy algal-dominated")+
+  annotate("text", x = 4.8, y = 0.1, label = "Calcifier-dominated")+
+  annotate("text", x = 4.8, y = -0.1, label = "Fleshy algal-dominated")+
   theme_bw()+
   theme(legend.direction = "horizontal",
         legend.position = c(.18, .1))
 p1
-ggsave(here("Output","CoralAlgae2022_15N.png"), p1, width = 10, height = 10)
+ggsave(here("Output","Calcifier_del15N.png"), p1, width = 10, height = 10)
 
 
 # percent N
-p2 <- redCover %>%
+p2 <- pCover %>%
   ggplot(aes(y = log((TotalCalc+1)/(TotalAlgae+1)), x = N_percent)) +
   geom_point() +
   geom_smooth(method = "lm", color = "black") +
@@ -109,17 +158,16 @@ p2 <- redCover %>%
   labs(y = "log ratio of calcifiers to fleshy algae",
        color = "",
        x = "%N (Nutrient Loading)") +
-  # annotate("text", x = 1.1, y = 0.5, label = "Calcifier-dominated")+
-  # annotate("text", x = 1.1, y = -0.5, label = "Fleshy algal-dominated")+
+  annotate("text", x = 1.0, y = 0.1, label = "Calcifier-dominated")+
+  annotate("text", x = 1.0, y = -0.1, label = "Fleshy algal-dominated")+
   theme_bw() +
   theme(legend.position = "none")
 p2
-ggsave(here("Output","CoralAlgae2022_Npercent.png"), p2, width = 10, height = 10)
+ggsave(here("Output","Calcifier_Npercent.png"), p2, width = 10, height = 10)
 
 
 # C:N ratio
-p3 <- redCover %>%
-  filter(CowTagID != "V17") %>%
+p3 <- pCover %>%
   ggplot(aes(y = log((TotalCalc+1)/(TotalAlgae+1)), x = C_N))+
   geom_point() +
   geom_smooth(method = "lm", color = "black") +
@@ -130,22 +178,24 @@ p3 <- redCover %>%
   labs(y = "log ratio of calcifiers to fleshy algae",
        color = "",
        x = "C:N Ratio (Nutrient Loading)") +
-  # annotate("text", x = 1.1, y = 0.5, label = "Calcifier-dominated")+
-  # annotate("text", x = 1.1, y = -0.5, label = "Fleshy algal-dominated")+
+  annotate("text", x = 33, y = 0.1, label = "Calcifier-dominated")+
+  annotate("text", x = 33, y = -0.1, label = "Fleshy algal-dominated")+
   theme_bw() +
   theme(legend.position = "none")
 p3
-ggsave(here("Output","CoralAlgae_CN.png"), p3, width = 10, height = 10)
+ggsave(here("Output","Calcifier_CN.png"), p3, width = 10, height = 10)
 
 
-modBenthic15N<-lm(log((TotalCalc+1)/(TotalAlgae+1))~del15N*Season, data = redCover)
+modBenthic15N<-lm(log((TotalCalc+1)/(TotalAlgae+1))~del15N, data = pCover)
 anova(modBenthic15N)
 
-modBenthicNpercent<-lm(log((TotalCalc+1)/(TotalAlgae+1))~N_percent*Season, data = redCover)
+modBenthicNpercent<-lm(log((TotalCalc+1)/(TotalAlgae+1))~N_percent, data = pCover)
 anova(modBenthicNpercent)
 
-modBenthicCN<-lm(log((TotalCalc+1)/(TotalAlgae+1))~C_N*Season, data = redCover)
+modBenthicCN<-lm(log((TotalCalc+1)/(TotalAlgae+1))~C_N, data = pCover)
 anova(modBenthicCN)
+
+
 
 #### Comparing Functional Groups across SGD ####
 ### Creating functions for plotting and linear regression modeling ###
@@ -209,6 +259,15 @@ pval(mydata = redCover, x = del15N, num = TotalCalc, den = TotalAlgae)
 
 plotfun(mydata = redCover, x = C_N, num = TotalCalc, den = TotalAlgae)
 pval(mydata = redCover, x = C_N, num = TotalCalc, den = TotalAlgae)
+
+
+
+
+
+
+
+
+
 
 
 
