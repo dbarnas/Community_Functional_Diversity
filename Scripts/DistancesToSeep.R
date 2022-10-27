f0e47d1db44c6e17ce1cabad37f741596faa6d09
Danielle Barnas <danielle.barnas@csun.edu>
2022-10-27 11:17:04 -0700

    add Cabral distances from seep GPS pt

--- Scripts/DistancesToSeep.R
+++ Scripts/DistancesToSeep.R
@@ -3,38 +3,38 @@
 #### LIBRARIES ####
 library(tidyverse)
 library(here)
-library(curl)
 library(geosphere)
 
 
 #### READ IN DATA ####
-survey<-read_csv(here("Data","Surveys","Varari_CC_Survey_reduced_taxon.csv"))
-traits<-read_csv(here("Data","Functional_Traits.csv"))
-turb<-read_csv(here("Data","Biogeochem","Turb_NC.csv"))
-AllChemData<-read_csv(curl('https://raw.githubusercontent.com/njsilbiger/MooreaSGD_site-selection/main/Data/August2021/Allbiogeochemdata_QC.csv'))
+locations <- read_csv(here("Data", "Sandwich_Locations_Final.csv"))
 
 
 ### CALCULATE DISTANCES TO SGD TREE
 
 # isolate seep lat and lon at Varari
-seepData <- AllChemData %>%
-  filter(Plate_Seep == 'Seep',
-         Location == 'Varari') %>%
-  select(CowTagID, lat, lon, Plate_Seep)
+seepData <- locations %>%
+  filter(Plate_Seep == 'Seep') %>%
+  select(Location, lat, lon) %>%
+  rename(lat_seep = lat,
+         lon_seep = lon)
 
 # isolate single numeric value for lat and lon
-seepLat <- as.numeric(seepData$lat[1])
-seepLon <- as.numeric(seepData$lon[1])
+# VseepLat <- as.numeric(seepData$lat[1])
+# VseepLon <- as.numeric(seepData$lon[1])
+# CseepLat <- as.numeric(seepData$lat[2])
+# CseepLon <- as.numeric(seepData$lon[2])
 
 # select distinct points for each plate location to calculate distances
-distData <- AllChemData %>%
-  filter(Plate_Seep == 'Plate',
-         Location == 'Varari') %>%
-  select(CowTagID, lat, lon, Top_Plate_ID, Plate_Seep) %>%
+distData <- locations %>%
+  filter(Plate_Seep == 'Plate') %>%
+  select(Location, CowTagID, lat, lon) %>%
   distinct() %>%
-  mutate(lat_seep = seepLat,
-         lon_seep = seepLon) %>%
-  # find Haversine distance
+  full_join(seepData)
+
+# find Haversine distances for each site
+Vdist <- distData %>%
+  filter(Location == "Varari") %>%
   mutate(dist_to_seep_m = distHaversine(cbind(lon_seep, lat_seep), cbind(lon, lat))) %>%
   # group by sample Site Number
   group_by(CowTagID) %>%
@@ -42,30 +42,38 @@ distData <- AllChemData %>%
   slice(which.min(dist_to_seep_m)) %>%
   select(-c(lat_seep, lon_seep))
 
-# isolate V13, which is in ambient upcurrent of SGD
-V13dist <- distData %>%
+Cdist <- distData %>%
+  filter(Location == "Cabral") %>%
+  mutate(dist_to_seep_m = distHaversine(cbind(lon_seep, lat_seep), cbind(lon, lat))) %>%
+  # group by sample Site Number
+  group_by(CowTagID) %>%
+  # choose only minimum distances
+  slice(which.min(dist_to_seep_m)) %>%
+  select(-c(lat_seep, lon_seep))
+
+# isolate V13, which is upcurrent of seep point
+V13dist <- Vdist %>%
   filter(CowTagID == 'V13') %>%
   mutate(dist_to_seep_m = -dist_to_seep_m) # get negative value because of opposite direction from other locations
+
 # remove V13 from distData then rejoin with new value from above
-distData <- distData %>%
+Vdist <- Vdist %>%
   filter(CowTagID != 'V13') %>%
   rbind(V13dist)
 
+# match seep point df with larger site df's
+seepData <- seepData %>%
+  mutate(CowTagID = c("VSEEP", "CSEEP"),
+         dist_to_seep_m = 0) %>%
+  rename(lat = lat_seep,
+         lon = lon_seep)
+
+# bind all df's together
+distData <- Vdist %>%
+  bind_rows(Cdist) %>%
+  bind_rows(seepData)
+
+# write csv
 write_csv(distData, here("Data", "Plate_Distance_to_Seep.csv"))
 
-# associate distance order to Top Plate ID order
-orderPlates <- distData %>%
-  ungroup() %>%
-  select(dist_to_seep_m, CowTagID) %>%
-  distinct() %>%
-  arrange(dist_to_seep_m) %>%
-  # as_factor creates levels based on current position
-  mutate(CowTagID = as_factor(as.character(CowTagID)))
 
-# sort factor levels by distance
-# distLevels <- paste(sort(as.numeric(levels(full_data$dist_to_seep_m))))
-distLevels <- paste(levels(orderPlates$CowTagID))
-
-# assign order to factor levels by distance
-distData$CowTagID <- factor(distData$CowTagID, levels = distLevels)
-levels(distData$CowTagID) # check
